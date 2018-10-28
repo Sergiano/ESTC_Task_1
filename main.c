ec4305303b0cde9243806e5ebafbfac2e24bbf79
Sergiano <kondroshsergiano@gmail.com>
2018-10-29 01:26:47 +0300

    Fixed output.

--- main.c
+++ main.c
@@ -8,12 +8,14 @@ int main()
 {
 	Somevector vect1, vect2, res;
 	
+	printf("\n");
+
 	vect1 = fillVector(1);
 	vect2 = fillVector(2);
 	
 	res = getSum(vect1, vect2);
 	
-	printf("SUM: %f %f %f", res.X1, res.X2, res.X3);
+	printf("SUM: %f %f %f\n", res.X1, res.X2, res.X3);
 	
 	return 0;
 }
@@ -27,5 +29,6 @@ Somevector fillVector(int number)
 	scanf("%f", &res.X2);
 	printf("X3 for 3D-vector %d> ", number);
 	scanf("%f", &res.X3);
+	printf("\n");
 	return res;
 }
