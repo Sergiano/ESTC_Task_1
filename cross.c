4294b95d521cd4e3e2a07859f543f22cdff43d52
Sergiano <kondroshsergiano@gmail.com>
2018-10-29 15:25:01 +0100

    Second iteration: implement a module for manipulating 3D-vectors (sum, sub, dot, cross, etc) using Makefile

---
+++ cross.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "somevector.h"
+
+Somevector getCross(Somevector vect1, Somevector vect2)
+{
+	Somevector res =
+	{
+		.X1 = vect1.X2 * vect2.X3 - vect1.X3 * vect2.X2,
+		.X2 = vect1.X1 * vect2.X3 - vect1.X3 * vect2.X1,
+		.X3 = vect1.X1 * vect2.X2 - vect1.X2 * vect2.X1
+	};
+	return res;
+}
+
+float getCrossModule(Somevector vect1, Somevector vect2)
+{
+	Somevector res = getCross(vect1, vect2);
+	return sqrt(res.X1 * res.X1 + res.X2 * res.X2 + res.X3 * res.X3);
+}
+
